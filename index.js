c5312cfc9263d51762c20aa50d9980b7e0ea0887
Faysal Akbar <faysalrj361@gmail.com>
2021-12-18 19:10:17 +0600

    more api added

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ async function run() {
         await client.connect();
         const database = client.db("uniqueMall");
         const productsCollection = database.collection("products");
+        const ordersCollection = database.collection("orders");
+        const reviewsCollection = database.collection("reviews");
+        const usersCollection = database.collection("users");
 
         // GET all products
         app.get('/products', async(req, res) => {
@@ -34,6 +37,66 @@ async function run() {
           const result = await productsCollection.findOne(query);
           res.send(result);
         })
+
+        // post api (insert orders)
+        app.post('/order', async(req, res) => {
+          const doc = req.body;
+          const result = await ordersCollection.insertOne(doc);
+          res.send(result);
+        })
+
+        // post user
+        app.post('/users', async(req, res) => {
+          const cursor = req.body;
+          const result = await usersCollection.insertOne(cursor);
+          res.send(result);
+        })
+
+        //get by email
+        app.get('/orders/:email', async(req, res) => {
+          const email = req.params.email;
+          const filter = {email: email}
+          const result = await ordersCollection.find(filter).toArray();
+          res.send(result);
+        });
+
+        // delete an order
+        app.delete('/orders/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const result = await ordersCollection.deleteOne(query);
+          res.json(result);
+        });
+
+        // post a review
+        app.post('/review', async(req, res) => {
+          const review = req.body;
+          const result = await reviewsCollection.insertOne(review);
+          res.json(result);
+        });
+
+        // post a product
+        app.post('/products', async(req, res) => {
+          const product = req.body;
+          const result = await productsCollection.insertOne(product);
+          res.json(result);
+        })
+
+        // get all orders
+        app.get('/orders', async(req, res) => {
+          const cursor = ordersCollection.find({});
+          const result = await cursor.toArray();
+          res.send(result);
+        })
+
+        // update an order status
+        app.put('/orders/:id', async(req, res) => {
+          const id = req.params.id;
+          const filter = {_id: ObjectId(id)}
+          const updateDoc = { $set: {status: 'shipped'}}
+          const result = await ordersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+      })
 
     }
     finally{
